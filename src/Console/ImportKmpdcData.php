f287eb156449ba688493d726e5359ec46b422dc7
kdbz <450845+kdbz@users.noreply.github.com>
2025-11-05 16:13:56 +0300

    bugfix: update package version in README and adjust default values for qualifications import

--- src/Console/ImportKmpdcData.php
+++ src/Console/ImportKmpdcData.php
@@ -183,8 +183,8 @@ class ImportKmpdcData extends Command
                 foreach ($row['qualifications'] as $qual) {
                     $degreeId = Degree::where('name', $qual['degree'] ?? '')->value('id');
                     $institutionId = Institution::where('name', $qual['institution'] ?? '')->value('id');
-                    $specialityName = $qual['speciality'] ?? 0;
-                    $year = $qual['year'] ?? null;
+                    $specialityName = $qual['speciality'] ?? '';
+                    $year = $qual['year'] ?? 0;
                     $qualification = Qualification::firstOrCreate(
                         [
                             'practitioner_id' => $practitionerId->id,
